7e67bfd212161b4780e8d89e22c48ba7717bd1c8
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-05-29 03:19:43 +0700

    update: JDBC(ResultSet)

---
+++ src/test/java/belajar/jdbc/database/ResultSetTest.java
@@ -0,0 +1,34 @@
+package belajar.jdbc.database;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ResultSetTest {
+    @Test
+    void testExecuteQuery() throws SQLException {
+        Connection connection = ConnectionUtil.getDataSource().getConnection();
+        Statement statement = connection.createStatement();
+
+        String sql = """
+                SELECT * FROM customers
+                """;
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        while(resultSet.next()) {
+            // Iterasi tiap data
+            String id = resultSet.getString("id");
+            String name = resultSet.getString("name");
+            String email = resultSet.getString("email");
+
+            System.out.println(String.join(",", id, name, email));
+        }
+
+        resultSet.close();
+        statement.close();
+        connection.close();
+    }
+}
